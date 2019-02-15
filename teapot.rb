7c69a624744d02e83d7d7a2d0e39036b8eb8217e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-15 20:17:08 +1300

    Modernize teapot config.

--- teapot.rb
+++ teapot.rb
@@ -20,16 +20,12 @@ end
 # Build Targets
 
 define_target 'time-library' do |target|
-	target.depends 'Build/Files'
-	target.depends 'Build/Clang'
-	
-	target.depends :platform
 	target.depends "Language/C++11", private: true
 	
 	target.provides "Library/Time" do
 		source_root = target.package.path + 'source'
 		
-		library_path = build prefix: environment.checksum, static_library: "Time", source_files: source_root.glob('Time/**/*.cpp')
+		library_path = build static_library: "Time", source_files: source_root.glob('Time/**/*.cpp')
 		
 		append linkflags library_path
 		append header_search_paths source_root
@@ -42,10 +38,10 @@ define_target "time-tests" do |target|
 	target.depends "Language/C++14", private: true
 	target.depends "Library/UnitTest"
 	
-	target.provides "Test/Time" do |arguments|
+	target.provides "Test/Time" do |*arguments|
 		test_root = target.package.path + 'test'
 		
-		run prefix: environment.checksum, source_files: test_root.glob('Time/**/*.cpp'), arguments: arguments
+		run tests: 'Time', source_files: test_root.glob('Time/**/*.cpp'), arguments: arguments
 	end
 end
 
